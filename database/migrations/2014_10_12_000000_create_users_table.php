a5b5c67e840424286557936eb428a9fc65c7a2f9
lehuy333333 <lehuy333333@gmail.com>
2023-07-03 09:27:19 +0700

    ac

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_middle_name');
+            $table->date('birthday');
             $table->string('email')->unique();
+            $table->string('phone_number');
+            $table->string('address');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
