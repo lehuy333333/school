322335edc603329c0d612987da2425b1a0b734ec
lehuy333333 <lehuy333333@gmail.com>
2023-08-29 21:28:22 +0700

    zxc

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+const path = require('path');
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
+                // 'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
-            refresh: true,
+            // refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
 });
