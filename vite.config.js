9dff964a2c0cfb3465a703669d54315e5eae888f
lehuy333333 <lehuy333333@gmail.com>
2023-08-29 21:31:15 +0700

    1

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                // 'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
-            // refresh: true,
         }),
     ],
     resolve: {
